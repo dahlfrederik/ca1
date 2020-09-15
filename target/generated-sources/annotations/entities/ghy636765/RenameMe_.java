737d4e0fd75403096335cb6d6d680e8133cb9fe7
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-15 10:13:23 +0200

    Gitignore

--- target/generated-sources/annotations/entities/ghy636765/RenameMe_.java
+++ target/generated-sources/annotations/entities/ghy636765/RenameMe_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-14T15:25:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T10:09:49")
 @StaticMetamodel(RenameMe.class)
 public class RenameMe_ { 
 
