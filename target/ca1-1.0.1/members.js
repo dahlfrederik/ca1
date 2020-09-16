54d56b4ad07ee499c0082bece30786319a4e40ff
thorchris <cph-tc147@cphbusiness.dk>
2020-09-16 10:34:26 +0200

    fetch fix

--- target/ca1-1.0.1/members.js
+++ target/ca1-1.0.1/members.js
@@ -8,17 +8,13 @@
              function fetchAllMembers(){
                  
                  let url = 'https://dachma.dk/ca1/api/groupmembers/all';
-                 let allMembers = document.getElementById("allMembers");
+                 let allMembers = document.getElementById("members");
                  
                  fetch(url)
                          .then(res => res.json())
                          .then(data => {
                              let newArray = data.map(x => `<tr><td>${x.name}</td><td>${x.id}</td><td>${x.favTvShow}</td></tr>`);
-                             allMembers.innerHTML =
-                                     `<table class="table table-bordered">
-                                        <thead><th>Name</th><th>ID</th><th>Favorite TV-show</th></thead>
-                                        ${newArray.join("")}
-                                    </table>`;
+                             allMembers.innerHTML = `${newArray.join("")}`;
                  });
              }
 
