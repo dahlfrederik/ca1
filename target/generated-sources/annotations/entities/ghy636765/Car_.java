00416452298ce4f1985bc0653c00220f167cc907
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-17 16:19:19 +0200

    Updated gitignore again
    We should put the entire target folder in there though.

--- target/generated-sources/annotations/entities/ghy636765/Car_.java
+++ target/generated-sources/annotations/entities/ghy636765/Car_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-17T15:34:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-17T15:55:42")
 @StaticMetamodel(Car.class)
 public class Car_ { 
 
